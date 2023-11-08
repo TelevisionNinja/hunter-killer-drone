68127ae2e8788e7ce9a1907e5335154a722a4460
TelevisionNinja <televisionninja@gmail.com>
2023-11-08 15:39:15 -0600

    fixed px4 gazebo terminal

--- workspace/src/hunter-killer-drone/hunter_killer_drone/processes.py
+++ workspace/src/hunter-killer-drone/hunter_killer_drone/processes.py
@@ -3,11 +3,15 @@
 
 import subprocess
 import time
+import os
 
+
+modelsPath = os.path.abspath("../models/")
 
 commands = [
     "MicroXRCEAgent udp4 -p 8888", # Micro XRCE-DDS Agent
-    "cd ~/PX4-Autopilot/ && make px4_sitl gz_x500" # PX4 SITL simulation
+    f"cd ~/PX4-Autopilot/ && GZ_SIM_RESOURCE_PATH={modelsPath} PX4_SYS_AUTOSTART=4002 PX4_GZ_MODEL_POSE=\"283.08,-136.22,3.86,0.00,0,-0.7\" PX4_GZ_MODEL=x500_depth ./build/px4_sitl_default/bin/px4", # PX4 SITL simulation
+    "source /opt/ros/humble/setup.bash && ros2 run ros_gz_image image_bridge /camera"
 ]
 
 for command in commands:
