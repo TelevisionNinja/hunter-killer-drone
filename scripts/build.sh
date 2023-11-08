d6791307535e68c7b46edee3312eb32b436cf24f
TelevisionNinja <televisionninja@gmail.com>
2023-11-08 16:22:51 -0600

    updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,8 @@ git clone https://github.com/PX4/px4_msgs.git
 cd ..
 source /opt/ros/humble/setup.bash
 colcon build
+
+# rename the original default.sdf file
+mv ~/PX4-Autopilot/Tools/simulation/gz/worlds/default.sdf ~/PX4-Autopilot/Tools/simulation/gz/worlds/default.sdf.original
+# use the default.sdf file from this repo
+cp ../worlds/default.sdf ~/PX4-Autopilot/Tools/simulation/gz/worlds/default.sdf
