731ffe2901f66f5492df322d03aa127b17509575
TelevisionNinja <televisionninja@gmail.com>
2024-05-06 14:25:33 -0500

    added fixed wing model

--- workspace/src/hunter-killer-drone/hunter_killer_drone/processes.py
+++ workspace/src/hunter-killer-drone/hunter_killer_drone/processes.py
@@ -8,9 +8,23 @@ import os
 
 modelsPath = os.path.abspath("../models/")
 
+# model = "fixed_wing"
+model = "x500_depth"
+
+# x
+# + back
+# - front
+
+# y
+# + left front
+# - right back
+pose = "283.08, -140, 3.86, 0, 0, -0.69"
+
+# PX4_SIM_MODEL v1.15
+# PX4_GZ_MODEL v1.14
 commands = [
     "MicroXRCEAgent udp4 -p 8888", # Micro XRCE-DDS Agent
-    f"cd ~/PX4-Autopilot/ && GZ_SIM_RESOURCE_PATH={modelsPath} PX4_SYS_AUTOSTART=4002 PX4_GZ_MODEL_POSE=\"283.08,-136.22,3.86,0.00,0,-0.7\" PX4_GZ_MODEL=x500_depth ./build/px4_sitl_default/bin/px4", # PX4 SITL simulation
+    f"cd ~/PX4-Autopilot/ && GZ_SIM_RESOURCE_PATH={modelsPath} PX4_SYS_AUTOSTART=4002 PX4_GZ_MODEL_POSE=\"{pose}\" PX4_GZ_MODEL={model} ./build/px4_sitl_default/bin/px4", # PX4 SITL simulation
     "source /opt/ros/humble/setup.bash && ros2 run ros_gz_image image_bridge /camera /depth_camera", # gazebo ros bridge
     "cd ~/QGroundControl/ && ./QGroundControl.AppImage" # QGroundControl
 ]
